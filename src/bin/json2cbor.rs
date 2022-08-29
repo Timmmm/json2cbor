b109224343e6786a130c5a758e06f0b3a39484a5
Jayshua Nelson <me@jayshuanelson.com>
2022-08-29 01:15:16 -0500

    Support stdin/stdout on json2cbor

--- src/bin/json2cbor.rs
+++ src/bin/json2cbor.rs
@@ -1,16 +1,34 @@
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+struct Args {
+    /// Input file to read from. Reads from stdin if not present.
+    #[clap(short, long)]
+    input: Option<String>,
+
+    /// Output file to write to. Writes to stdout if not present.
+    #[clap(short, long)]
+    output: Option<String>,
+}
 
 fn main() -> Result<()> {
-    let args = std::env::args().collect::<Vec<_>>();
-    if args.len() != 3 {
-        eprintln!("Usage: json2cbor <input.json> <output.cbor>");
-        return Ok(())
-    }
+    let args = Args::parse();
 
-    let fin = File::open(&args[1])?;
-    let fout = File::create(&args[2])?;
+    let fin: Box<dyn std::io::Read> =
+        match args.input {
+            None => Box::new(std::io::stdin()),
+            Some(file_name) => Box::new(File::open(file_name)?),
+        };
+
+    let fout: Box<dyn std::io::Write> =
+        match args.output {
+            None => Box::new(std::io::stdout()),
+            Some(file_name) => Box::new(File::create(file_name)?),
+        };
 
     let reader = BufReader::new(fin);
     let writer = BufWriter::new(fout);
