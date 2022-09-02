11c56c6ba758ddb549867dbf9649f92d39656de1
Jayshua <me@jayshuanelson.com>
2022-09-02 06:20:20 -0500

    Support stdin and stdout
    Adds support for reading from stdin and writing to stdout.
    
    Since the input/output arguments are optional now, I switched to using the -o flag to indicate the output file. Parsing provided by clap which I added as a dependency.
    
    Since that change isn't backwards-compatible, I incremented the major version number.
    
    I also upgraded dependencies to their latest versions and relaxed the version requirements for serde_cbor and serde-transcode so they could upgrade to latest as well. It doesn't appear a specific patch version is required by the code for those two dependencies.

--- src/bin/cbor2json.rs
+++ src/bin/cbor2json.rs
@@ -1,16 +1,34 @@
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+struct Args {
+    /// Input file to read from. Reads from stdin if not present.
+    #[clap(value_parser, id="INPUT_FILE")]
+    input: Option<String>,
+
+    /// Output file to write to. Writes to stdout if not present.
+    #[clap(short, long, id="OUTPUT_FILE")]
+    output: Option<String>,
+}
 
 fn main() -> Result<()> {
-    let args = std::env::args().collect::<Vec<_>>();
-    if args.len() != 3 {
-        eprintln!("Usage: cbor2json <input.cbor> <output.json>");
-        return Ok(())
-    }
+    let args = Args::parse();
 
-    let fin = File::open(&args[1])?;
-    let fout = File::create(&args[2])?;
+    let fin: Box<dyn std::io::Read> =
+        match args.input {
+            None => Box::new(std::io::stdin()),
+            Some(file_name) => Box::new(File::open(file_name)?),
+        };
+
+    let fout: Box<dyn std::io::Write> =
+        match args.output {
+            None => Box::new(std::io::stdout()),
+            Some(file_name) => Box::new(File::create(file_name)?),
+        };
 
     let reader = BufReader::new(fin);
     let writer = BufWriter::new(fout);
